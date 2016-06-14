e107a5c79b35b08e3e4ab1d2b37a8f977bcafc0b
Ted Pattison <ted@tedpattison.net>
2016-06-14 17:37:11 -0400

    Updates

---
+++ Modules/07_ProviderHostedApps/Lab/Solution/CustomerManager_Exercise2/CustomerManagerWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using CustomerManagerWeb.Filters;
+
+namespace CustomerManagerWeb {
+  public class FilterConfig {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+      filters.Add(new HandleErrorAttribute());
+      filters.Add(new SharePointSessionManagerFilterAttribute());
+    }
+  }
+}
---
+++ Modules/07_ProviderHostedApps/Lab/Solution/CustomerManager_Exercise2/CustomerManagerWeb/Filters/SharePointSessionManagerFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using CustomerManagerWeb.Models;
+
+namespace CustomerManagerWeb.Filters {
+  public class SharePointSessionManagerFilterAttribute : ActionFilterAttribute {
+
+    public override void OnActionExecuting(ActionExecutingContext filterContext) {
+      SharePointSessionManager.InitializeRequest(filterContext.Controller);
+    }
+
+  }
+}
